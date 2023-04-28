66eb13eca0ad4a700576706bcadde5da0310a6a6
Nagy Alexandra <alexandranagy.13@yahoo.com>
2023-04-28 17:47:21 +0300

    Updated servcies.

--- ProjectManagement/ProjectManagement.DataAccess.EF/StudentRepository.cs
+++ ProjectManagement/ProjectManagement.DataAccess.EF/StudentRepository.cs
@@ -16,29 +16,30 @@ namespace ProjectManagement.DataAccess.EF
 
         public Student GetStudentById(Guid studentId)
         {
-            var student = this.context.Set<Student>().FirstOrDefault(s=>s.Id == studentId);    
+            var student = this.context.Set<Student>().FirstOrDefault(s => s.Id == studentId);
             return student;
         }
-        
-        public List<Discipline> GetDisciplinesOfStudent (Guid studentId)
+
+        public List<Discipline> GetDisciplinesOfStudent(Guid studentId)
         {
             var student = GetStudentById(studentId);
-            var disciplines = student.StudentDisciplines.Select(sd=>sd.Discipline).ToList();
+            var disciplines = student.StudentDisciplines.Select(sd => sd.Discipline).ToList();
             return disciplines;
         }
-        public List<Project> GetProjectsOfStudentOfDiscipline(Guid studentId,Guid disciplineId)
+        public List<Project> GetProjectsOfStudentOfDiscipline(Guid studentId, Guid disciplineId)
         {
             var student = GetStudentById(studentId);
-            var studentProjects = student.StudentProjects.Select(sp=>sp.Project).ToList();
+            var studentProjects = student.StudentProjects.Select(sp => sp.Project).ToList();
             var studentProjectsOfDiscipline = studentProjects
-                .Where(sp=>sp.Discipline.Id == disciplineId)
+                .Where(sp => sp.Discipline.Id == disciplineId)
                 .ToList();
             return studentProjectsOfDiscipline;
         }
         public IEnumerable<Question> GetQuestionsOfDiscipline(Guid disciplineId)
         {
-            var discipline = this.context.Set<Discipline>().First(d=>d.Id == disciplineId); 
-            return discipline.Questions;    
+            var discipline = this.context.Set<Discipline>().First(d => d.Id == disciplineId);
+            return discipline.Questions;
         }
+
     }
 }
