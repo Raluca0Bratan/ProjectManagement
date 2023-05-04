b981a9e28c3a9efa83e42e468c9353ef0e85f484
Nagy Alexandra <alexandranagy.13@yahoo.com>
2023-05-04 22:00:23 +0300

    Added ViewAccountInfo controller and view.

---
+++ ProjectManagement/ProjectManagement/Controllers/ViewInfoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.DataAccess.EF;
+using ProjectManagement.Logic;
+
+namespace ProjectManagement.Controllers
+{
+    public class ViewInfoController : Controller
+    {
+        private readonly TeacherService teacherService;
+        public ViewInfoController(TeacherService teacherService)
+        {
+            this.teacherService = teacherService;
+        }
+        public IActionResult Index(Guid userId)
+        {
+            var user = teacherService.GetTeacherById(userId);
+            return View(user);
+
+        }
+    }
+}
